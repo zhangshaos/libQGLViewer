687f9662512622e023372d7fc1bfb8c8280ea019
zxm <chanceyzhang@qq.com>
2024-01-16 17:10:04 +0800

    first commit

--- examples/fastDraw/fastDraw.h
+++
@@ -1,9 +0,0 @@
-#include <QGLViewer/qglviewer.h>
-
-class Viewer : public QGLViewer {
-protected:
-  virtual void init();
-  virtual void draw();
-  virtual void fastDraw();
-  virtual QString helpString() const;
-};
--- examples/manipulatedFrame/manipulatedFrame.h
+++
@@ -1,8 +0,0 @@
-#include <QGLViewer/qglviewer.h>
-
-class Viewer : public QGLViewer {
-protected:
-  virtual void init();
-  virtual void draw();
-  virtual QString helpString() const;
-};
